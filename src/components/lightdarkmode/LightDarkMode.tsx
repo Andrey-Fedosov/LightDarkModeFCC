96780c59e51bc0a0e6b817c961b6765f49d12ee9
Andrey-Fedosov <andrefedosov@gmail.com>
2024-02-28 00:36:06 +0300

    feature: add light-dark mode logic

--- src/components/lightdarkmode/LightDarkMode.tsx
+++ src/components/lightdarkmode/LightDarkMode.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import "./theme.css";
+import { useLocalStorage } from "./useLocalStorage";
 
 export function LightDarkMode() {
-  return <div>It's light dark mode</div>;
+  const [theme, setTheme] = useLocalStorage("theme", "light");
+
+  function changeThemeHandler() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  console.log(theme);
+
+  return (
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <h2>It's light dark mode</h2>
+        <button onClick={changeThemeHandler}>Change color</button>
+      </div>
+    </div>
+  );
 }
