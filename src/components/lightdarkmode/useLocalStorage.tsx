98f1d53c4a4a6c226ac91b79dd96382888d32f3c
Andrey-Fedosov <andrefedosov@gmail.com>
2024-03-28 23:51:11 +0300

    feature: implement theme change functionality

---
+++ src/components/lightdarkmode/useLocalStorage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export function useLocalStorage(key: string, defaultValue: string) {
+  const [value, setValue] = useState(() => {
+    let currentValue;
+
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || String(defaultValue)
+      );
+    } catch (error) {
+      console.log(error);
+      currentValue = defaultValue;
+    }
+    return currentValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
